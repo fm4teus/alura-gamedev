47c221d57a9b0cbe48f5d4f8d653abfb4d5f64c4
fmateus <fm4teus@gmail.com>
2020-06-30 14:50:57 -0300

    added background and music

--- sketch.js
+++ sketch.js
@@ -1,7 +1,55 @@
-function setup() {
-  createCanvas(400, 400);
+let imagemCenario;
+let imagemPersonagem;
+let cenario;
+let trilhaSonora;
+
+function preload() {
+  imagemCenario = loadImage('imagens/cenario/floresta.png');
+  imagemPersonagem = loadImage('imagens/personagem/correndo.png');
+  trilhaSonora = loadSound('sons/trilha_jogo.mp3')
 }
 
-function draw() {
-  background(220);
+function setup() {
+  getAudioContext().suspend();
+  createCanvas(800, 450);
+  cenario = new Cenario(imagemCenario,5); 
+  trilhaSonora.play();
+}
+
+function draw() { 
+  cenario.exibe();
+  cenario.move();
+  image(imagemPersonagem,0,height-135,110,135,0,0,220,270);
+  circle(mouseX,mouseY,50);
+}
+
+function mousePressed() {
+  userStartAudio();
+}
+
+class Cenario {
+  constructor(imagem, velocidade){
+    this.imagem = imagem;
+    this.velocidade = velocidade;
+    this.x1 = 0;
+    this.x2 = width;
+  }
+
+  exibe(){
+    image(this.imagem, this.x1,0,width,height);
+    image(this.imagem, this.x2 + 1,0,width,height);
+  }
+
+  move(){
+    this.x1 = this.x1 - this.velocidade;
+    this.x2 = this.x2 - this.velocidade;
+    
+    if(this.x1 < -width){
+      this.x1 = width;
+    }
+
+    if(this.x2< -width){
+      this.x2 = width; 
+    }
+  }
 }
